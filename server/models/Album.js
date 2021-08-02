af724d5daa3638c36519048cc5874d812ee50181
Steve Dever <stephendever.cr@gmail.com>
2021-08-02 12:41:18 -0400

    add models albumjs first take

---
+++ server/models/Album.js
@@ -0,0 +1,16 @@
+const { Schema } = require('mongoose');
+
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedAlbums` array in User.js
+const albumSchema = new Schema({
+  albumId: {
+    type: String,
+    required: true,
+  },
+ 
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = albumSchema;
