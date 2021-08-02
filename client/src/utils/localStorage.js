ee42db7f06eca77a36fb43264c4f20a70bf5fcd9
Steve Dever <stephendever.cr@gmail.com>
2021-08-02 10:31:58 -0400

    add utiils localStoragejs

---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,31 @@
+export const getSavedAlbumIds = () => {
+    const savedAlbumIds = localStorage.getItem('saved_albums')
+      ? JSON.parse(localStorage.getItem('saved_albums'))
+      : [];
+  
+    return savedAlbumIds;
+  };
+  
+  export const saveAlbumIds = (albumIdArr) => {
+    if (albumIdArr.length) {
+      localStorage.setItem('saved_album', JSON.stringify(albumIdArr));
+    } else {
+      localStorage.removeItem('saved_albums');
+    }
+  };
+  
+  export const removeAlbumId = (albumId) => {
+    const savedAlbumIds = localStorage.getItem('saved_albums')
+      ? JSON.parse(localStorage.getItem('saved_albums'))
+      : null;
+  
+    if (!savedAlbumIds) {
+      return false;
+    }
+  
+    const updatedSavedAlbumIds = savedAlbumIds?.filter((savedAlbumId) => savedAlbumId !== albumId);
+    localStorage.setItem('saved_albums', JSON.stringify(updatedSavedAlbumIds));
+  
+    return true;
+  };
+  
