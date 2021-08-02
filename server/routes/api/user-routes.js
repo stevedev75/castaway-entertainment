f935f56b0c74315f2319cface2189e48ed075d59
Steve Dever <stephendever.cr@gmail.com>
2021-08-02 12:57:15 -0400

    add routes api userroutesjs

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveAlbum,
+  deleteAlbum,
+  login,
+} = require('../../controllers/user-controller');
+
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, saveAlbum);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/albums/:albumId').delete(authMiddleware, deleteAlbum);
+
+module.exports = router;
