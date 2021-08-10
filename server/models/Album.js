4237f11ee3e4e1578fcdb6a2178dd43f62003a04
Steve Dever <stephendever.cr@gmail.com>
2021-08-10 10:42:49 -0400

    add change to albumjs

--- server/models/Album.js
+++ server/models/Album.js
@@ -11,6 +11,27 @@ const albumSchema = new Schema({
     type: String,
     required: true,
   },
-});
+} , { _id: false } );
 
 module.exports = albumSchema;
+
+
+
+
+
+// const { Schema } = require('mongoose');
+
+// // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedAlbums` array in User.js
+// const albumSchema = new Schema({
+//   albumId: {
+//     type: String,
+//     required: true,
+//   },
+ 
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// module.exports = albumSchema;
